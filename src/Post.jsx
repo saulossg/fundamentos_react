1c0d714b7b561fa5753efa50c7f01ece0d6342bf
t_saulo.gomes@hapvida.com.br <saulo.da.silva.gomes@accenture.com>
2024-04-27 23:29:24 -0300

    feat: centering the header

--- src/Post.jsx
+++ src/Post.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export function Post(props) {
     return (
         <div>
-            <strong>Author: {props.author}</strong>
+            <strong>Author: {props.author}</strong><br/>
             <strong>Content: {props.content}</strong>
         </div>
     )
